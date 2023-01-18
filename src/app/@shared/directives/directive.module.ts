6b531595613b248a305bc70dbb98dcdc445dcad1
zahra.shokrian <zahra.shokrian70@gmail.com>
2023-01-18 12:17:40 +0330

    some style changed

--- src/app/@shared/directives/directive.module.ts
+++ src/app/@shared/directives/directive.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {OnlynumberDirective} from "@shared/directives/numeric-textbox";
 
 
 @NgModule({
   declarations: [
-    OnlynumberDirective
   ],
   imports: [
     CommonModule
   ], exports:[
-    OnlynumberDirective
+
   ]
 })
 export class DirectiveModule { }
